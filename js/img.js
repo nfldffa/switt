012c3ef189ddaa158aa48b2e74dfe66219e087e2
nfldffa <erlangga.nde@gmail.com>
2025-03-12 22:03:23 +0700

    final

--- js/img.js
+++ js/img.js
@@ -2,23 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const gambarContainer = document.getElementById("gambar-container");
 
   const gambarList = [
-      { src: "image/lonsum.webp", caption: "PT. Perusahaan Perkebunan London Sumatra Indonesia, Tbk" , link: "perusahaan/lonsum.html"},
-      { src: "image/dami.webp", caption: "PT. Dami Mas Sejahtera" , link: "perusahaan/dami.html"},
-      { src: "image/bakrie.webp", caption: "PT. ASD-Bakrie Oil Palm Seed Indonesia" , link: "perusahaan/asd.html"},
+      { src: "https://photos.idnfinancials.com/static/web/2025/lonsum%201.JPG", caption: "PT. Perusahaan Perkebunan London Sumatra Indonesia, Tbk" , link: "perusahaan/lonsum.html"},
+      { src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM5s1e76QR_Np2F62F-lDZjEktVOr5dXlf2g&s", caption: "PT. Dami Mas Sejahtera" , link: "perusahaan/dami.html"},
+      { src: "https://www.indoconnex.com/public/uploads/pbd_business/655d6abc50525.jpg", caption: "PT. ASD-Bakrie Oil Palm Seed Indonesia" , link: "perusahaan/asd.html"},
       { src: "image/ehsan.webp", caption: "PT. Sasaran Ehsan Mekarsari" , link: "perusahaan/ehsan.html"},
       { src: "image/yunus.webp", caption: "PT. Tunggal Yunus Estate" , link: "perusahaan/yunus.html"},
-      { src: "image/bina.webp", caption: "PT. Bina Sawit Makmur" , link: "perusahaan/bina.html"},
-      { src: "image/ppks.webp", caption: "PT. Pusat Penelitian Kelapa Sawit (PPKS)" , link: "perusahaan/ppks.html"},
-      { src: "image/Socfin.webp", caption: "PT. Socfin Indonesia" , link: "perusahaan/socfin.html"},
-      { src: "image/MITRA AGRO.webp", caption: "PT Mitra Agro Servindo" , link: "perusahaan/mitra.html"},
-      { src: "image/BAKTI TANI.webp", caption: "PT. Bakti Tani Nusantara" , link: "perusahaan/tani.html"},
-      { src: "image/Deli.webp", caption: "PT. Timbang Deli Indonesia" , link: "perusahaan/deli.html"},
-      { src: "image/Palma.webp", caption: "PT. Palma Inti Lestari" , link: "perusahaan/palma.html"},
-      { src: "image/Tania.webp", caption: "PT. Tania Selatan : Oil Palm Plantation" , link: "perusahaan/tania.html"},
-      { src: "image/PTPNIV.webp", caption: "PTPN IV" , link: "perusahaan/ptp.html"},
-      { src: "image/SLG.webp", caption: "PT. Aneka Sawit Lestari " , link: "perusahaan/aneka.html"},
-      { src: "image/AAR.webp", caption: "PT.AAR Indonesia" , link: "perusahaan/aar.html"},
-      { src: "image/PANCA SURYA.webp", caption: "PT.Panca Surya Garden" , link: "perusahaan/panca.html"},
+      { src: "https://lokermedan.net/wp-content/uploads/2024/07/PT-Sampoerna-Agro-Tbk.webp", caption: "PT. Bina Sawit Makmur" , link: "perusahaan/bina.html"},
+      { src: "https://cakapinterview.com/wp-content/uploads/2020/06/Pusat-Penelitian-Kelapa-Sawit-PPKS.jpg", caption: "PT. Pusat Penelitian Kelapa Sawit (PPKS)" , link: "perusahaan/ppks.html"},
+      { src: "https://media.licdn.com/dms/image/v2/C560BAQGtsS0-ZXcISQ/company-logo_200_200/company-logo_200_200/0/1631376548272?e=2147483647&v=beta&t=_MSAzoKgaIYNv8Z3vYN4X2XxGBhwwJS5XfzioGV9744", caption: "PT. Socfin Indonesia" , link: "perusahaan/socfin.html"},
+      { src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWbA-x2dsmVkjIbPyppyYWHF9SsT460ncC8w&s", caption: "PT Mitra Agro Servindo" , link: "perusahaan/mitra.html"},
+      { src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Bso9ALW4nD-Qmg9YqPRkCAIoHL7V8C_vzw&s", caption: "PT. Bakti Tani Nusantara" , link: "perusahaan/tani.html"},
+      { src: "https://media.licdn.com/dms/image/v2/C561BAQEYi-g_95yN3g/company-background_10000/company-background_10000/0/1644287863801/verdantbioscience_cover?e=2147483647&v=beta&t=Fg2J4gplJWpCae3GQh58rvG4DdAjOcu5KvCoBWrZiqs", caption: "PT. Timbang Deli Indonesia" , link: "perusahaan/deli.html"},
+      { src: "https://ugc.production.linktr.ee/A5HUEFQPeu2ncs6RhgiH_5Xym5aZ8TkFQnXjy?io=true&size=avatar-v3_0", caption: "PT. Palma Inti Lestari" , link: "perusahaan/palma.html"},
+      { src: "https://loker.pasarpanduan.com/wp-content/uploads/2022/02/Lowongan-Kerja-PT-Tania-Selatan.jpg", caption: "PT. Tania Selatan : Oil Palm Plantation" , link: "perusahaan/tania.html"},
+      { src: "https://www.bersamabumn.com/wp-content/uploads/2024/09/Lowongan-Kerja-BUMN-PT-Perkebunan-Nusantara-IV.webp", caption: "PTPN IV" , link: "perusahaan/ptp.html"},
+      { src: "https://www.cakerja.com/wp-content/uploads/2023/12/Lowongan-Kerja-PT-Sawit-Lestari-Group.webp", caption: "PT. Aneka Sawit Lestari " , link: "perusahaan/aneka.html"},
+      { src: "https://media.licdn.com/dms/image/v2/C560BAQHhTd4S4lVp4Q/company-logo_200_200/company-logo_200_200/0/1674794808318?e=2147483647&v=beta&t=2K_Inh8rhdjhFf5Ti2EXGGGqY7mnsNoIjxozoZoy-nI", caption: "PT.AAR Indonesia" , link: "perusahaan/aar.html"},
+      { src: "https://assets.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/akurat/gallerybiro/2023/02/big/img_63f571326e6f40-15478173-64159872.jpg", caption: "PT.Panca Surya Garden" , link: "perusahaan/panca.html"},
   ];
 
   gambarList.forEach(item => {
--- js/imgberanda.js
+++ js/imgberanda.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const sawitContainer = document.getElementById("sawit-container");
 
   const gambarList = [
-      "image/p1.webp",
-      "image/p2.webp",
-      "image/p3.webp",
-      "image/p4.webp",
-      "image/p5.webp",
-      "image/p6.webp"
+      "https://st2.depositphotos.com/1015741/7809/i/450/depositphotos_78092108-stock-photo-palm-oil-tree.jpg",
+      "https://beritainvestor.id/wp-content/uploads/2023/10/1683101596-1240x826-1.webp",
+      "https://www.astra-agro.co.id/wp-content/uploads/2018/03/ash-pict-003-1080x675-1080x540.jpg",
+      "https://asset.kompas.com/crops/JgcFK-JMIQgJMYjEP5Yzfw9rr9E=/115x0:964x566/1200x800/data/photo/2020/06/29/5ef940fe73ccc.jpg",
+      "https://widya.ai/wp-content/uploads/2022/11/Buah-Kelapa-Sawit-1024x683.jpg",
+      "https://www.asianagri.com/wp-content/uploads/2021/06/business-img-3.jpg"
   ];
 
   gambarList.forEach(src => {
